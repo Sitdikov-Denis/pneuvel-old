b3ffd32df60db56219b68395a984547e5555a5de
Denis Sitdikov <denis.sitdikov95@yandex.ru>
2023-09-09 23:15:08 +0400

    fix: webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.tsx', // Ваш файл с точкой входа
+  entry: './src/index.tsx', // Ваша точка входа TypeScript/JSX
   output: {
-    filename: 'bundle.js', // Название бандла
-    path: path.resolve(__dirname, 'dist'), // Путь для сохранения бандла
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/, // Регулярное выражение для файлов TypeScript/JSX
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader', // Используем ts-loader
+        },n
+      },
+      {
+        test: /\.jsx?$/, // Регулярное выражение для файлов JavaScript/JSX
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // Используем babel-loader
+          options: {
+            presets: ['@babel/preset-react'], // Подключаем пресет React
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'], // Добавьте расширения для TypeScript и JSX
   },
 };
